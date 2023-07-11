73c485612ee0c360f5da27505c804fb6e662a230
{ElenaKovaleva} <{lenshemeta@gmail.com}>
2023-07-11 03:44:13 +0300

    ДЗ-1 выполнено

--- src/main/java/ru/otus/ElenaKovaleva/Main.java
+++ src/main/java/ru/otus/ElenaKovaleva/Main.java
@@ -36,7 +36,7 @@ public class Main {
         int data = 12;
         if (data < 10) {
             System.out.println("Красный");
-        } else if (data >= 10 || data <= 20) {
+        } else if (data > 10 || data < 20) {
             System.out.println("Желтый");
         } else {
             System.out.println("Зеленый");
