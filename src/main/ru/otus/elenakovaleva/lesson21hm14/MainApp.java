269fb7d88d14131ea9be8f7867129716a09f5f95
{ElenaKovaleva} <{lenshemeta@gmail.com}>
2023-10-06 13:05:19 +0300

    дз14урок21 исправлено

--- src/main/ru/otus/elenakovaleva/lesson21hm14/MainApp.java
+++ src/main/ru/otus/elenakovaleva/lesson21hm14/MainApp.java
@@ -3,16 +3,9 @@ package ru.otus.elenakovaleva.lesson21hm14;
 public class MainApp {
     public static void main(String[] args) throws Exception {
         MyDoubleArray myDoubleArray = new MyDoubleArray();
-        //System.out.println(myDoubleArray.fillArr()); // 19580
-        /*Thread t1 = new Thread(() -> myDoubleArray.fill1());
-        Thread t2 = new Thread(() -> myDoubleArray.fill2());
-        Thread t3 = new Thread(() -> myDoubleArray.fill3());
-        Thread t4 = new Thread(() -> myDoubleArray.fill4());*/
+        myDoubleArray.fillArrRandom();
 
-        long startTime = System.currentTimeMillis();
-        myDoubleArray.fillMain();
-        long endTime = System.currentTimeMillis();
-        long resTime3 = endTime - startTime;
-        System.out.println(resTime3); //5
+        System.out.println("Время выполнения цикла: " + myDoubleArray.fillArr()); // 16395
+        System.out.println("Время выполнения потоков: " + myDoubleArray.fillArrWithThreads());//1
     }
 }
--- src/main/ru/otus/elenakovaleva/lesson21hm14/MyDoubleArray.java
+++ src/main/ru/otus/elenakovaleva/lesson21hm14/MyDoubleArray.java
@@ -6,74 +6,40 @@ public class MyDoubleArray {
     public MyDoubleArray() {
     }
 
+    public double[] fillArrRandom() {
+        for (int i = 0; i < arr.length; i++) {
+        arr[i] = Math.random();
+        }
+        return arr;
+    }
+
     public Long fillArr() {
         long startTime = System.currentTimeMillis();
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = Math.random();
-        }
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i/1.2);
+            arr[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
         }
         long endTime = System.currentTimeMillis();
-        long resTime = endTime - startTime;
-        return resTime;
+        return endTime - startTime;
     }
 
-    public double[] fill1() {
-        for (int i = 0; i < arr.length / 4; i++) {
-            arr[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i/1.2);
-        }
-        return arr;
-    }
-
-    public double[] fill2() {
-        for (int i = arr.length / 4; i < arr.length / 2; i++) {
-            arr[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i/1.2);
-        }
-        return arr;
-    }
-
-    public double[] fill3() {
-        for (int i = arr.length/2; i < arr.length - (arr.length / 4); i++) {
-            arr[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i/1.2);
-        }
-        return arr;
-    }
-
-    public double[] fill4() {
-        for (int i = arr.length - (arr.length / 4); i < arr.length; i++) {
-            arr[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i/1.2);
-        }
-        return arr;
-    }
-
-    public double[] fillMain() {
-        for (int i =0; i < 4; i++) {
-            Thread t1 = new Thread(() -> {
-                for (int j = 0; j < arr.length / 4; j++) {
-                    arr[j] = 1.14 * Math.cos(j) * Math.sin(j * 0.2) * Math.cos(j/1.2);
-                }
-            });
-            Thread t2 = new Thread(() -> {
-                for (int j = arr.length / 4; j < arr.length / 2; j++) {
+    public Long fillArrWithThreads() {
+        Thread[] arrays = new Thread[4];
+        for (int i = 0; i < 4; i++) {
+            int inc = i;
+            Thread ti = new Thread(() -> {
+                int startIndex = (arr.length / 4) * inc;
+                int endIndex = (arr.length / 4) * (inc + 1);
+                for (int j = startIndex; j < endIndex; j++) {
                     arr[j] = 1.14 * Math.cos(j) * Math.sin(j * 0.2) * Math.cos(j / 1.2);
                 }
             });
-            Thread t3 = new Thread(() -> {
-                for (int j = arr.length/2; j < arr.length - (arr.length / 4); j++) {
-                    arr[j] = 1.14 * Math.cos(j) * Math.sin(j * 0.2) * Math.cos(j/1.2);
-                }
-            });
-            Thread t4 = new Thread(() -> {
-                for (int j = arr.length - (arr.length / 4); j < arr.length; j++) {
-                    arr[j] = 1.14 * Math.cos(j) * Math.sin(j * 0.2) * Math.cos(j/1.2);
-                }
-            });
-            t1.start();
-            t2.start();
-            t3.start();
-            t4.start();
+            arrays[i] = ti;
         }
-        return arr;
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < arrays.length; i++) {
+            arrays[i].start();
+        }
+        long endTime = System.currentTimeMillis();
+        return endTime - startTime;
     }
 }
